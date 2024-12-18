03a26ce0fae1c3207909212074e644f6699344fa
diego <diegozaratean@gmail.com>
2024-12-18 18:50:00 +0000

    finish react 2 class

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 // 4. importar componente
 import Nav from "./nav";
+import Jumbotron from "./jumbotron";
+import Button from "./button";
 // 5. utilizar componente
 
 const Home = () => {
 	return (
 		<>
 			<Nav/>
+			<Jumbotron />
 			<h1 className="text-center mt-5">Hola Bienvenidos a React 123 :)</h1>
 			<p>Lorem ipsum dolor sit amet.</p>
+			<Button />
 			<h2>footer</h2>
 		</>
 	);
---
+++ src/js/components/button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ()=>{
+    return(
+        <button className="btn btn-danger px-5 mb-5" type="button">
+            Compra mi nuevo libro
+        </button>
+    )
+}
+
+export default Button
